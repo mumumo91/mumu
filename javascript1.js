b76d41b8a22d25586cb8a4598a00005ed94335f6
mumumo91 <65005902+mumumo91@users.noreply.github.com>
2020-05-27 16:35:12 +0900

    12th version

--- javascript1.js
+++ javascript1.js
@@ -1,12 +1,3 @@
-
-$(function(){
-  $("#header").load("header.html");
-    $("#footer").load("footer.html");
-});
-
-
-
-
 $(function(){
 $('.要素１ li').hover(function(){
   $("ul:not(:animated)", this).slideDown();
