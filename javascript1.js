1770ffd9f75472106c4a9cd1125bfabc7bf82423
mumumo91 <65005902+mumumo91@users.noreply.github.com>
2020-05-31 17:46:41 +0900

    24th vertion

--- javascript1.js
+++ javascript1.js
@@ -1,3 +1,14 @@
+$(function (){
+$('.要素１ li').hover(function(){
+  $("ul:not(:animated)", this).slideDown();
+},
+function(){
+$("ul.dropdown",this).slideUp();
+});
+});
+
+
+
 
 $(function (){
 $('.kamoku li').hover(function(){
