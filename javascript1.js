9b77b55e00fc2aec6a8edd9a2310b4413cdbad4e
mumumo91 <65005902+mumumo91@users.noreply.github.com>
2020-05-31 21:48:38 +0900

    28th version

--- javascript1.js
+++ javascript1.js
@@ -1,5 +1,5 @@
 $(function (){
-$('header .要素１ li').hover(function(){
+$('#header .要素１ li a').hover(function(){
   $("ul:not(:animated)", this).slideDown();
 },
 function(){
@@ -11,7 +11,7 @@ $("ul.dropdown",this).slideUp();
 
 
 $(function (){
-$('sideber.kamoku li').hover(function(){
+$('#sideber .kamoku li a').hover(function(){
   $("ul:not(:animated)", this).slideDown();
 },
 function(){
